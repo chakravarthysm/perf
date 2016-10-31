9952ea627c650b7350b83018e9a122c50052dee6
Chakravarthy S M <chakravarthy.sm@netapp.com>
2016-10-31 21:18:12 +0530

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+extern crate hyper;
+
+use hyper::client;
+fn main(){
+let client = client::Client::new();
+
+let res = client.get("http://example.domain").send().unwrap();
+assert_eq!(res.status, hyper::Ok);
+}
